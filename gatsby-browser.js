fbb1d449bd2a6fa6d8adb929f63adbb9b0bd12cd
Roland Branten <roland.branten@gmail.com>
2020-03-04 13:42:09 +0100

    removed prismic

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-const { registerLinkResolver } = require("gatsby-source-prismic-graphql");
-
-registerLinkResolver(require("./utils/linkResolver").linkResolver);
--- utils/linkResolver.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  linkResolver(doc) {
-    return "/";
-  }
-};
