747e09fc70c12238abd49b8520fa766e8c5f3cc5
Roland Branten <roland.branten@gmail.com>
2020-03-11 15:46:36 +0100

    changed from svg to png

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,5 @@
 // Components==============
-import logo from "assets/logo.svg";
+import logo from "assets/logo.png";
 import { Container } from "mixins";
 import React from "react";
 import styled from "styled-components";
